1e8aa6767e3aa985f20e3cb6f76ba44608d16d25
RPodemski <70305161+RPodemski@users.noreply.github.com>
2021-02-13 22:16:47 -0600

    update

--- 17-Mapping-Web/Instructions/static/js/logic.js
+++ 17-Mapping-Web/Instructions/static/js/logic.js
@@ -30,7 +30,8 @@ function createMap() {
     });
 }
 
-function build(earthData) { //couldn't add plateData
+//couldn't add plateData
+function build(earthData) {
     var darkMode = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
